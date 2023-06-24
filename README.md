72684b629d599cb27822578abdf2fa9ea3ada124
daurenabasov <daurenabasov987@gmail.com>
2023-06-25 03:24:07 +0600

    variables

---
+++ README.md
@@ -0,0 +1,21 @@
+# Типы данных Dart  10
+
+- Numbers (int, double)
+- Strings (String)
+- Booleans (bool)
+- Records ((value1, value2))
+- Lists (List, also known as arrays)
+- Sets (Set)
+- Maps (Map)
+- Runes (Runes; often replaced by the characters API)
+- Symbols (Symbol)
+- The value null (Null)
+
+
+
+# List - это массивы
+# Sets- это коллекции, которые хранят в себе уникальные значения (не можем  ложить повторные значения)
+# Runes - это из чего состоят строки 
+
+
+- в чем разница между float и double = это одно  и тоже float 32b && double 64b 
---
+++ bin/variables.dart
@@ -0,0 +1,45 @@
+void main(List<String> arguments) {
+  // var a = false;
+  // String b = "hello";
+  // int c = 10;
+  // bool g = true;
+  // final w = "salam";
+  // const number = 10; 
+
+// # Математические операторы 
+
+// var b = 4 + 4;
+// var c = 4 + 9 / 4 * 2 / 1;
+
+
+// Операторы
+
+bool t = 4>=5  && 6 == 6 || 7<=9; // true
+
+print(t);
+
+// конкатенция 
+
+ const a = "10";
+  final b = "world";
+    
+  print("$a $b");
+
+// StingBuffer()
+
+//  const list = ["hello", "world"];
+//     var kontan = StringBuffer();
+  
+//   list.forEach((item){
+//         kontan.write(item);
+//   });
+//   var fff = kontan.toString();
+  
+//   print(fff);
+
+var d = int.tryParse("2233dfsfdsfdsfdsfds");
+
+print(d);
+  
+  
+}
